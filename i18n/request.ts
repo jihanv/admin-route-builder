203122f12679b03e3f13f4110305092514c77d3a
jihanv <61974348+jihanv@users.noreply.github.com>
2026-06-06 21:49:20 +0900

    Load the English messages file in next-intl.

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,5 +2,5 @@ import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => ({
   locale: "en",
-  messages: {},
+  messages: (await import("../messages/en.json")).default,
 }));
