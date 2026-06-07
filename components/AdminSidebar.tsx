1d4edc72b8c60ab84152956243ce15aa729ef706
jihanv <61974348+jihanv@users.noreply.github.com>
2026-06-07 09:49:05 +0900

    Add admin sidebar component

---
+++ components/AdminSidebar.tsx
@@ -0,0 +1,9 @@
+import { Sidebar } from "@/components/ui/sidebar";
+
+export function AdminSidebar() {
+  return (
+    <Sidebar>
+      <div className="p-4 font-semibold">REI Mission</div>
+    </Sidebar>
+  );
+}
