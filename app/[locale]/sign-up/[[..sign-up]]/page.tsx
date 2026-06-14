75dcfe681e22d065cf998dca4637b89da2862504
jihanv <61974348+jihanv@users.noreply.github.com>
2026-06-14 19:05:35 +0900

    Add signup and signin page

---
+++ app/[locale]/sign-up/[[..sign-up]]/page.tsx
@@ -0,0 +1,30 @@
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import { useLocale } from "next-intl";
+
+export default function Page() {
+  const locale = useLocale();
+
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome!</h1>
+          <p className="text-base text-[#7E8CA0]">Create your account!</p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp signInUrl={`/${locale}/signin`} />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground"></Loader2>
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-neutral-700 hidden lg:flex items-center justify-center">
+        <Image src="/icon.svg" height={100} width={100} alt="Logo" />
+      </div>
+    </div>
+  );
+}
