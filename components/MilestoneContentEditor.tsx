8dab1f095191ec8e84494d2c709f783f4dd6baa3
jihanv <61974348+jihanv@users.noreply.github.com>
2026-07-08 20:44:55 +0900

    Upload resized milestone images

--- components/MilestoneContentEditor.tsx
+++ components/MilestoneContentEditor.tsx
@@ -74,7 +74,7 @@ export function MilestoneContentEditor({
 
     setImageFilesByMilestoneId((currentFiles) => ({
       ...currentFiles,
-      [milestoneId]: imageFile,
+      [milestoneId]: resizedImageFile,
     }));
 
     setImagePreviewUrlsByMilestoneId((currentPreviewUrls) => ({
